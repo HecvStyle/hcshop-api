6e19d0892ad7d6fadcdc81e3a388316884e8f816
hecv <hecvstyle@foxmail.com>
2021-08-30 17:49:38 +0800

    商品web层更新
    修改配置文件

--- goods-web/api/goods/goods.go
+++ goods-web/api/goods/goods.go
@@ -1,12 +1,16 @@
 package goods
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"hcshop-api/goods-web/global"
+	"hcshop-api/goods-web/proto"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -65,5 +69,52 @@ func removeTopStruct(fields map[string]string) map[string]string {
 }
 
 func List(ctx *gin.Context) {
+	request := &proto.GoodsFilterRequest{}
+	priceMin := ctx.DefaultQuery("p_min", "0")
+	priceMinInt, _ := strconv.Atoi(priceMin)
+	request.PriceMin = int32(priceMinInt)
 
+	priceMax := ctx.DefaultQuery("p_max", "0")
+	priceMaxInt, _ := strconv.Atoi(priceMax)
+	request.PriceMax = int32(priceMaxInt)
+
+	isNew := ctx.DefaultQuery("is_new", "0")
+	if isNew == "1" {
+		request.IsNew = true
+	}
+
+	isTab := ctx.DefaultQuery("is_tab", "1")
+	if isTab == "1" {
+		request.IsTab = true
+	}
+	categoryId := ctx.DefaultQuery("category_id", "0")
+	categoryIdInt, _ := strconv.Atoi(categoryId)
+	request.TopCategory = int32(categoryIdInt)
+
+	pages := ctx.DefaultQuery("pages", "0")
+	pagesInt, _ := strconv.Atoi(pages)
+	request.Pages = int32(pagesInt)
+
+	pageNums := ctx.DefaultQuery("page_nums", "10")
+	pageNumsInt, _ := strconv.Atoi(pageNums)
+	request.PagePerNums = int32(pageNumsInt)
+
+	keyword := ctx.DefaultQuery("key_words", "")
+	request.KeyWords = keyword
+
+	brandId := ctx.DefaultQuery("brand_id", "0")
+	brandIdInt, _ := strconv.Atoi(brandId)
+	request.Brand = int32(brandIdInt)
+
+	resp, err := global.GoodsSrvClient.GoodsList(context.Background(), request)
+	if err != nil {
+		zap.S().Errorw("商品【List】查询失败")
+		HandlerGrpcErrorToHttp(err, ctx)
+		return
+	}
+	reMap := map[string]interface{}{
+		"total": resp.Total,
+		"data":  resp.Data,
+	}
+	ctx.JSON(http.StatusOK, reMap)
 }
--- goods-web/config/config.go
+++ goods-web/config/config.go
@@ -17,6 +17,8 @@ type ConsulConfig struct {
 type ServerConfig struct {
 	Name         string         `mapstructure:"name" json:"name"`
 	Port         int            `mapstructure:"port" json:"port"`
+	Tags         []string       `mapstructure:"tags" json:"tags"`
+	Host         string         `mapstructure:"host" json:"host"`
 	GoodsSrvInfo GoodsSrvConfig `mapstructure:"goods_srv" json:"goods_srv"`
 	JWTInfo      JWTConfig      `mapstructure:"jwt" json:"jwt"`
 	ConsulInfo   ConsulConfig   `mapstructure:"consul" json:"consul"`
