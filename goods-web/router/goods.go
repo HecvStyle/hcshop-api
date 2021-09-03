e68cbe1586e39a009886d319b56f5b30dcc7c717
hecv <hecvstyle@foxmail.com>
2021-09-03 15:19:05 +0800

    接口测试
    接口测试

--- goods-web/router/goods.go
+++ goods-web/router/goods.go
@@ -10,7 +10,8 @@ func InitGoodsRouter(Router *gin.RouterGroup) {
 	GoodsRouter := Router.Group("goods")
 	{
 		GoodsRouter.GET("", goods.List)
-		GoodsRouter.POST("", middlewares.JWTAuth(), middlewares.IsAdminAuth(), goods.New)
+		//GoodsRouter.POST("", middlewares.JWTAuth(), middlewares.IsAdminAuth(), goods.New)
+		GoodsRouter.POST("",goods.New)
 		GoodsRouter.GET("/:id", goods.Detail)
 		GoodsRouter.DELETE("/:id", middlewares.JWTAuth(), middlewares.IsAdminAuth(), goods.Delete)
 
